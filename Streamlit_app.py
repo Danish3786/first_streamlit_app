62c456b91971c7da203da512bc85c86d07546e0f
Danish3786 <133586067+Danish3786@users.noreply.github.com>
2023-05-14 20:54:08 +0530

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,3 +1,6 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
-streamlit.title('Danish')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 and Blurberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free_Range Egg')
